d1ab37ba6055b16776ac7c090b293d075dcfe738
duhnuker <dinukanh@gmail.com>
2024-08-30 16:34:45 +1000

    jwtGenerator

---
+++ server/utils/jwtGenerator.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
+
+interface UserPayload {
+    user: {
+        id: string;
+    };
+}
+
+function jwtGenerator(user_id: string) {
+    const payload: UserPayload = {
+        user: {
+            id: user_id
+        }
+    };
+
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in the environment variables");
+      }
+
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1hr" });
+}
+
+export default jwtGenerator;
