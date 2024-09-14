96b0e9d630ba7e2e4ddba7187d8fb4f642de449d
duhnuker <dinukanh@gmail.com>
2024-09-14 21:25:04 +1000

    VERY basic layout and styling Login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -45,33 +45,40 @@ const Login = ({ setAuth }: { setAuth: (auth: boolean) => void }) => {
   };
 
   return (
-    <div>
-      <div>
-        <h1>Welcome back,</h1>
-        <form onSubmit={onSubmitForm}>
-          <input
-            type='email'
-            name='email'
-            placeholder='Email'
-            value={email}
-            onChange={e => onChange(e)}>
-          </input>
-          <br></br>
-          <input
-            type='password'
-            name='password'
-            placeholder='Password'
-            value={password}
-            onChange={e => onChange(e)}>
-          </input>
-          <br></br>
-          <div>
-            <button>Login</button>
-          </div>
-        </form>
-        <p>Don't have an account?</p>
+    <div className='h-screen w-full animated-background bg-gradient-to-r from-midnightp1 via-midnightp1 to-midnightp2 flex items-center justify-center'>
+      <div className='flex justify-evenly w-full'>
         <div>
-          <Link to="/register">Register here!</Link>
+          <h1 className='text-5xl pb-20 text-midnightp2 font-bold'>Welcome back,</h1>
+          <form onSubmit={onSubmitForm}>
+            <input
+              className='mt-4 rounded-lg bg-black text-white'
+              type='email'
+              name='email'
+              placeholder='Email'
+              value={email}
+              onChange={e => onChange(e)}>
+            </input>
+            <br></br>
+            <input
+              className='mt-4 rounded-lg bg-black text-white'
+              type='password'
+              name='password'
+              placeholder='Password'
+              value={password}
+              onChange={e => onChange(e)}>
+            </input>
+            <br></br>
+            <div>
+              <button className='mt-3 py-2 px-4 rounded-2xl border-2 border-red-900 bg-black text-white hover:bg-red-950 hover:text-red-100 transition-colors duration-300'>Login</button>
+            </div>
+          </form>
+          <p className='text-white pt-10'>Don't have an account?</p>
+          <div className='text-white pt-4'>
+            <Link to="/register" className='py-2 px-4 rounded-2xl border-2 border-red-900 bg-black text-white hover:bg-red-950 hover:text-red-100 transition-colors duration-300'>Register here!</Link>
+          </div>
+        </div>
+        <div className='text-white bg-transparent'>
+          <img src='compAnIonlogo.jpg' className='w-[400px] h-[400px] opacity-20'></img>
         </div>
       </div>
     </div>
