1eedb6765c17023a2bfe2a96803f65af9273d9a4
duhnuker <dinukanh@gmail.com>
2024-09-15 13:43:49 +1000

    SUPER BASIC Register.tsx styling and structure

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -40,46 +40,52 @@ const Register = ({ setAuth }: { setAuth: (auth: boolean) => void }) => {
       }
     }
   };
-
-  return (
-    <div>
-      <div>
-        <h1>Register</h1>
-        <form onSubmit={onSubmitForm}>
-          <input
-            type='text'
-            name='name'
-            placeholder='Full Name'
-            value={name}
-            onChange={e => onChange(e)}
-          />
-          <br />
-          <input
-            type='email'
-            name='email'
-            placeholder='Email'
-            value={email}
-            onChange={e => onChange(e)}
-          />
-          <br />
-          <input
-            type='password'
-            name='password'
-            placeholder='Password'
-            value={password}
-            onChange={e => onChange(e)}
-          />
-          <div className="text-center">
-            <button>Submit</button>
+    return (
+      <div className='h-screen w-full animated-background bg-gradient-to-r from-midnightp1 via-midnightp1 to-midnightp2 flex items-center justify-center'>
+        <div className='flex justify-evenly w-full'>
+          <div>
+            <h1 className='text-5xl pb-20 text-midnightp2 font-bold'>Join us,</h1>
+            <form onSubmit={onSubmitForm}>
+              <input
+                className='mt-4 rounded-lg bg-black text-white'
+                type='text'
+                name='name'
+                placeholder='Full Name'
+                value={name}
+                onChange={e => onChange(e)}
+              />
+              <br />
+              <input
+                className='mt-4 rounded-lg bg-black text-white'
+                type='email'
+                name='email'
+                placeholder='Email'
+                value={email}
+                onChange={e => onChange(e)}
+              />
+              <br />
+              <input
+                className='mt-4 rounded-lg bg-black text-white'
+                type='password'
+                name='password'
+                placeholder='Password'
+                value={password}
+                onChange={e => onChange(e)}
+              />
+              <div>
+                <button className='mt-3 py-2 px-4 rounded-2xl border-2 border-red-900 bg-black text-white hover:bg-red-950 hover:text-red-100 transition-colors duration-300'>Register</button>
+              </div>
+            </form>
+            <p className='text-white pt-10'>Already have an account?</p>
+            <div className='text-white pt-4'>
+              <Link to="/login" className='py-2 px-4 rounded-2xl border-2 border-red-900 bg-black text-white hover:bg-red-950 hover:text-red-100 transition-colors duration-300'>Login here!</Link>
+            </div>
           </div>
-        </form>
-        <p>Already have an account?</p>
-        <div>
-          <Link to="/login">Login</Link>
+          <div className='text-white bg-transparent'>
+            <img src='compAnIonlogo.jpg' className='w-[400px] h-[400px] opacity-20' alt="compAnIon logo" />
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
-
+    )
+  }
 export default Register;
