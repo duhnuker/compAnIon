25e17b44245639716fd30f3967daf1ad967674dd
duhnuker <dinukanh@gmail.com>
2024-10-14 15:39:33 +1100

    YourProgress only accessible if authenticated FIX

--- client/src/pages/YourProgress.tsx
+++ client/src/pages/YourProgress.tsx
@@ -1,6 +1,24 @@
-import DataChart from '../components/DataChart'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DataChart from '../components/DataChart';
 
 const YourProgress = () => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.token) {
+      navigate('/login')
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [navigate]);
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
   return (
     <div>
       <h1>Your progress</h1>
